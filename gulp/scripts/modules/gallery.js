da28ff86b7b6916db14826cb45b9823d3eff7dbe
Karl Tynan <hello@karltynan.co.uk>
2018-04-18 17:22:57 +0100

    Temp gallery for bx slider styles

---
+++ gulp/scripts/modules/gallery.js
@@ -0,0 +1,26 @@
+﻿var gallery = {
+
+	init: function () {
+		$('.m-gallery__menu:not(.js-gallery-done)').each(function () {
+			var wrapper = $(this);
+
+			// init
+			wrapper.bxSlider({
+				infiniteLoop: false,
+				hideControlOnEnd: true,
+				speed: 1000
+			});
+
+			// done
+			wrapper.addClass('js-gallery-done');
+		});
+	}
+
+};
+
+$(function () {
+	gallery.init();
+	site.doc.ajaxComplete(function () {
+		gallery.init();
+	});
+});
