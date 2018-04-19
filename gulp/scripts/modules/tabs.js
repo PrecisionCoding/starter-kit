5444755fa48197e68fe6ca7b1ecb677a23a1f002
Karl Tynan <hello@karltynan.co.uk>
2018-04-19 15:11:43 +0100

    Tabs

---
+++ gulp/scripts/modules/tabs.js
@@ -0,0 +1,52 @@
+﻿var tabs = {
+
+	init: function () {
+
+		$('.js-tabs:not(.js-tabs-done)').each(function () {
+			var wrapper = $(this);
+			var tabsLinks = wrapper.find('.js-tabs-link');
+			var tabsContent = wrapper.find('.js-tabs-tab');
+
+			// change
+			var changeTab = function (id, scroll) {
+				var newTab = wrapper.find(id);
+				// update links
+				tabsLinks.removeClass('active').filter('[href="' + id + '"]').addClass('active');
+				// update content
+				tabsContent.hide().filter(id).show();
+				// scroll
+				if (scroll) {
+					site.scrollTo(newTab.offset().top);
+				}
+			};
+
+			// init
+			if (tabsLinks.filter('.active').length == 1) {
+				changeTab(tabsLinks.filter('.active').attr('href'), false);
+			}
+			else {
+				changeTab(tabLinks.first().attr('href'), false);
+			}
+
+			// links
+			tabsLinks.off('click').on('click', function () {
+				var link = $(this);
+				if (link.is('.active')) return false;
+				changeTab(link.attr('href'), false);
+				return false;
+			});
+
+			// done
+			wrapper.addClass('js-tabs-done');
+		});
+
+	}
+
+};
+
+$(function () {
+	tabs.init();
+	site.doc.ajaxComplete(function () {
+		tabs.init();
+	});
+});
---
+++ gulp/scripts/modules/takeover-link.js
@@ -0,0 +1,36 @@
+﻿var takeoverLink = {
+
+	init: function () {
+
+		$('.js-takeover-link:not(.js-takeover-link-done)').each(function () {
+			var link = $(this);
+			var id = $('a' + link.attr('data-takeover-id'));
+
+			if (!id.length) return false;
+
+			// click
+			link.off('click').on('click', function () {
+				// click
+				id.click();
+				// scroll to
+				if (link.attr('data-takeover-scroll')) {
+					site.scrollTo($(link.attr('data-takeover-scroll')).offset().top);
+				}
+				
+				return false;
+			});
+
+			// done
+			link.addClass('js-takeover-link-done');
+		});
+
+	}
+
+};
+
+$(function () {
+	takeoverLink.init();
+	site.doc.ajaxComplete(function () {
+		takeoverLink.init();
+	});
+});
